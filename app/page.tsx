6220df472aedec138d5b637cc61a177d5536fba5
Florian Bardin <fbardin51@gmail.com>
2025-10-21 23:42:07 +0200

    Ajustement padding vertical

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "../src/components/navbar";
 
 export default function Home() {
   return (
-    <div className="px-8 py-5 md:py-8 sm:px-10 md:px-16 lg:px-28">
+    <div className="px-8 py-6 md:py-8 sm:px-10 md:px-16 lg:px-28">
       <Navbar />
     </div>
   );
