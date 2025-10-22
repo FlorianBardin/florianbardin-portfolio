2ca0bc3730f8ed196b778ac647a1f530608af7b0
Florian Bardin <fbardin51@gmail.com>
2025-10-22 22:25:39 +0200

    Implémentation du composant header dans la page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Header from "./header";
 import Navbar from "./navbar";
 
 export default function Home() {
   return (
     <div className="px-8 py-6 md:py-8 sm:px-10 md:px-16 lg:px-28">
       <Navbar />
+      <Header />
     </div>
   );
 }
