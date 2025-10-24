c194897b2bdabc5105df173de350f27182196bcc
Florian Bardin <fbardin51@gmail.com>
2025-10-24 10:43:31 +0200

    intégration du composant Timeline dans la partie Journey

--- app/journey.tsx
+++ app/journey.tsx
@@ -1,9 +1,10 @@
+import Timeline from "@/components/timeline";
 import Image from "next/image";
 import React from "react";
 
 const Journey = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <div className="flex gap-2 items-center lg:gap-3">
         <Image
           className="h-4 w-4 lg:h-5 lg:w-5"
@@ -14,6 +15,7 @@ const Journey = () => {
         />
         <h3>Parcours</h3>
       </div>
+      <Timeline />
     </div>
   );
 };
