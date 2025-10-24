c8ea2fcbeede3ccb9a56ee20bdc7ce8490076e46
Florian Bardin <fbardin51@gmail.com>
2025-10-24 11:49:50 +0200

    Création d'une partie projet

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Header from "./header";
 import Journey from "./journey";
 import Navbar from "./navbar";
+import Projects from "./projects";
 
 export default function Home() {
   return (
-    <div className="px-8 py-6 md:py-8 sm:px-10 md:px-16 lg:px-28">
+    <div className="px-8 py-6 space-y-6 md:py-8 sm:px-10 md:px-16 lg:px-28">
       <Navbar />
       <Header />
       <Journey />
+      <Projects />
     </div>
   );
 }
---
+++ app/projects.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+import { ProjectsGrid } from "@/components/projects-grid";
+
+const Projects = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex gap-2 items-center lg:gap-3">
+        <Image
+          className="h-4 w-4 lg:h-5 lg:w-5"
+          width={17}
+          height={17}
+          src="projects.svg"
+          alt="Projects logo"
+        />
+        <h3>Projets</h3>
+      </div>
+      <ProjectsGrid />
+    </div>
+  );
+};
+
+export default Projects;
