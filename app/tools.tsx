3a227a147ada601502aec2a11e61cd166b2a5023
Florian Bardin <fbardin51@gmail.com>
2025-10-25 14:10:13 +0200

    Création du composant de partie Outils

---
+++ app/tools.tsx
@@ -0,0 +1,44 @@
+import ToolsSection from "@/components/tools-section";
+import { Badge } from "@/components/ui/badge";
+import {
+  backTools,
+  dataTools,
+  frontTools,
+  otherTools,
+  softwareTools,
+} from "@/lib/tools";
+import Image from "next/image";
+import React from "react";
+
+const Tools = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-between lg:justify-start lg:gap-4">
+        <div className="flex items-center gap-2 lg:gap-3">
+          <Image
+            className="h-4 w-4 lg:h-5 lg:w-5"
+            width={17}
+            height={17}
+            src="tools.svg"
+            alt="Tools logo"
+          />
+          <h3>Outils</h3>
+        </div>
+        <div className="space-x-2">
+          <Badge>Utilisation régulière</Badge>
+          <Badge variant="secondary">Utilisation occasionel</Badge>
+        </div>
+      </div>
+
+      <div className="grid gap-8 lg:grid-cols-2">
+        <ToolsSection title="🎨 Front-end" tools={frontTools} />
+        <ToolsSection title="🛠️ Back-end" tools={backTools} />
+        <ToolsSection title="🗄️ Base de données" tools={dataTools} />
+        <ToolsSection title="🧰 Logiciels" tools={softwareTools} />
+        <ToolsSection title="Autres" tools={otherTools} />
+      </div>
+    </div>
+  );
+};
+
+export default Tools;
