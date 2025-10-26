4f11e236618fab93159c7f1b0fb6bff17610a238
Florian Bardin <fbardin51@gmail.com>
2025-10-26 16:08:24 +0100

    Ajouts des images de demo dans les cases projet

--- src/components/projects-grid.tsx
+++ src/components/projects-grid.tsx
@@ -1,21 +1,22 @@
 import { BentoCard, BentoGrid } from "@/components/ui/bento-grid";
-import {
-  BookUser,
-  Database,
-  Gamepad2,
-  Satellite,
-  SquareKanban,
-} from "lucide-react";
+import { BookUser, Gamepad2, Satellite, SquareKanban } from "lucide-react";
+import { Safari } from "./ui/safari";
 
 const features = [
   {
     Icon: BookUser,
-    name: "Portfolio",
+    name: "florianbardin.dev",
     description: "Portfolio réalisé avec Next.js",
-    href: "/",
+    href: "https://github.com/FlorianBardin/florianbardin-portfolio",
     cta: "Voir sur GitHub",
-    background: <img className="absolute -top-20 -right-20 opacity-60" />,
-    className: "md:col-span-1 lg:row-span-2 lg:col-start-1 lg:col-end-1",
+    background: (
+      <Safari
+        url="florianbardin.dev"
+        imageSrc="portfolio-demo.png"
+        className="absolute top-10 lg:top-20 origin-top scale-85 mask-[linear-gradient(to_top,transparent_10%,#000_100%)] transition-all duration-300 ease-out group-hover:scale-90"
+      />
+    ),
+    className: "md:col-span-2 lg:col-start-1 lg:col-end-1",
   },
   {
     Icon: Satellite,
@@ -24,8 +25,14 @@ const features = [
       "Application web de visualisation des données de l'API APOD de la Nasa.",
     href: "https://github.com/FlorianBardin/APOD-viewer",
     cta: "Voir sur GitHub",
-    background: <img className="absolute -top-20 -right-20 opacity-60" />,
-    className: "md:col-span-1 lg:col-start-2 lg:col-end-2 lg:row-span-2",
+    background: (
+      <Safari
+        url="apod-viewer-iota.vercel.app"
+        imageSrc="apod-demo.png"
+        className="absolute top-10 lg:top-20 origin-top scale-85 mask-[linear-gradient(to_top,transparent_10%,#000_100%)] transition-all duration-300 ease-out group-hover:scale-90"
+      />
+    ),
+    className: "md:col-span-2 lg:col-start-2 lg:col-end-2",
   },
   {
     Icon: SquareKanban,
@@ -34,37 +41,36 @@ const features = [
       "Application web permettant de gérer des tableaux Kanban dans le stockage local du navigateur.",
     href: "https://github.com/FlorianBardin/task-flow",
     cta: "Voir sur GitHub",
-    background: <img className="absolute -top-20 -right-20 opacity-60" />,
-    className:
-      "md:col-span-2 lg:col-start-3 lg:col-span-2 lg:row-start-1 lg:row-end-1",
+    background: (
+      <Safari
+        url="task-flow-nine-alpha.vercel.app"
+        imageSrc="taskflow-demo.png"
+        className="absolute top-10 lg:top-20 origin-top scale-85 mask-[linear-gradient(to_top,transparent_10%,#000_100%)] transition-all duration-300 ease-out group-hover:scale-90"
+      />
+    ),
+    className: "md:col-span-1 lg:col-start-3 lg:col-end-3",
   },
   {
     Icon: Gamepad2,
     name: "Steam Game CRUD",
     description:
       "Application web CRUD PHP réalisé en duo dans le cadre d'une SAÉ de BUT INFO.",
-    href: "/",
+    href: "https://github.com/FlorianBardin/steam-game-crud",
     cta: "Voir sur GitHub",
-    background: <img className="absolute -top-20 -right-20 opacity-60" />,
-    className:
-      "md:col-span-1 lg:col-start-3 lg:col-end-3 lg:row-start-2 lg:row-end-2",
-  },
-  {
-    Icon: Database,
-    name: "Base de données JO 2024",
-    description:
-      "Mise en place d'une base de données dans le cadre des jeux olympiques de Paris.",
-    href: "/",
-    cta: "Voir sur GitHub",
-    background: <img className="absolute -top-20 -right-20 opacity-60" />,
-    className:
-      "md:col-span-1 lg:col-start-4 lg:col-end-4 lg:row-start-2 lg:row-end-2",
+    background: (
+      <Safari
+        url="steam-game-crud.app"
+        imageSrc="steamcrud-demo.png"
+        className="absolute top-10 lg:top-20 origin-top scale-85 mask-[linear-gradient(to_top,transparent_10%,#000_100%)] transition-all duration-300 ease-out group-hover:scale-90"
+      />
+    ),
+    className: "md:col-span-1 lg:col-start-4 lg:col-end-4",
   },
 ];
 
 export function ProjectsGrid() {
   return (
-    <BentoGrid className="grid md:grid-cols-2 lg:grid-cols-4 lg:grid-rows-2">
+    <BentoGrid className="grid md:grid-cols-2 lg:grid-cols-4">
       {features.map((feature) => (
         <BentoCard key={feature.name} {...feature} />
       ))}
