1aa4f6932a897f5739b0f8e7c7bc6bc7ade91bdf
Florian Bardin <fbardin51@gmail.com>
2025-10-27 19:13:38 +0100

    Implémentation timeline cards

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,29 +1,13 @@
 import React from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "./ui/card";
-import { Badge } from "./ui/badge";
 import { steps } from "@/lib/steps";
+import TimelineCard from "./timeline-card";
 
 const Timeline = () => {
   const timelineSteps = steps;
   return (
-    <div className="grid md:grid-cols-4 gap-3">
+    <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-3">
       {timelineSteps.map((step, i) => (
-        <Card key={i}>
-          <CardHeader>
-            <div className="flex flex-col-reverse justify-start gap-5">
-              <CardTitle>{step.title}</CardTitle>
-              <Badge variant="secondary">{step.date}</Badge>
-            </div>
-            <CardDescription>{step.location}</CardDescription>
-          </CardHeader>
-          {step.desc ? <CardContent>{step.desc}</CardContent> : ""}
-        </Card>
+        <TimelineCard key={i} delay={i} step={step} />
       ))}
     </div>
   );
