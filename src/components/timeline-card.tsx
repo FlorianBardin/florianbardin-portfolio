31b6670d32cca5441c21d4a510e0083c4e86d4fa
Florian Bardin <fbardin51@gmail.com>
2025-10-27 19:13:22 +0100

    Création de carte pour chacun des steps de la timeline

---
+++ src/components/timeline-card.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+import AnimatedContent from "./AnimatedContent";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Badge } from "./ui/badge";
+import { stepType } from "@/lib/steps";
+
+type TimelineCardProps = {
+  step: stepType;
+  delay: number;
+};
+
+const TimelineCard = ({ step, delay }: TimelineCardProps) => {
+  return (
+    <AnimatedContent delay={0.1 * delay}>
+      <Card className="h-full">
+        <CardHeader>
+          <div className="flex flex-col-reverse justify-start gap-5">
+            <CardTitle>{step.title}</CardTitle>
+            <Badge variant="secondary">{step.date}</Badge>
+          </div>
+          <CardDescription>{step.location}</CardDescription>
+        </CardHeader>
+        {step.desc ? <CardContent>{step.desc}</CardContent> : ""}
+      </Card>
+    </AnimatedContent>
+  );
+};
+
+export default TimelineCard;
