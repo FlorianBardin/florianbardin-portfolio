949cb513cb7dc00890f379698f63d6bd82c6b5a1
Florian Bardin <fbardin51@gmail.com>
2025-10-29 00:41:13 +0100

    Mise à jour des imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { cn } from "@/lib/utils";
-import Footer from "./footer";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
