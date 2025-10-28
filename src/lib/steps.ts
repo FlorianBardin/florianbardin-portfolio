715d4903108b0b52272a750aa932d06150faad97
Florian Bardin <fbardin51@gmail.com>
2025-10-28 22:37:57 +0100

    Mise à jour des données de la timeline

--- src/lib/steps.ts
+++ src/lib/steps.ts
@@ -10,22 +10,22 @@ export const steps: stepType[] = [
     title: "Baccalauréat Général 📚",
     date: "Juin 2024",
     location: "Lycée Polyvalent Stéphane Hessel, Epernay",
-    desc: "Mathématiques, Numérique et sciences de l'informatique",
+    desc: "Mathématiques, Numérique et Sciences de l'Informatique",
   },
   {
     title: "BUT Informatique 1ère année 👨‍💻",
     date: "Septembre 2024 - Juin 2025",
-    location: "IUT Reims-Chalons-Charleville, Reims",
+    location: "IUT Reims-Châlons-Charleville, Reims",
   },
   {
     title: "Cuviste 🍾",
-    date: "Juillet - Aout 2025",
-    location: "Champagne Bollinger, Ay",
+    date: "Juillet - Août 2025",
+    location: "Champagne Bollinger, Aÿ",
   },
   {
     title: "BUT Informatique 2ème année 👨‍💻",
     date: "Septembre 2025 - Juin 2026",
-    location: "IUT Reims-Chalons-Charleville, Reims",
+    location: "IUT Reims-Châlons-Charleville, Reims",
     desc: "Spécilisation développement (C++)",
   },
 ];
