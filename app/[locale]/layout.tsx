e0299ce4b602aedd252d18e1b986b4416189f4ce
Florian Bardin <fbardin51@gmail.com>
2026-03-03 21:50:13 +0100

    feat(i18n): added a route to specify current app language

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { ThemeProvider } from "next-themes";
 import { cn } from "@/lib/utils";
 import Footer from "@/components/footer";
