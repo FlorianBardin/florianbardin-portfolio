c96370b1a58e98523f8f6af7ea494a4072aa9586
Florian Bardin <fbardin51@gmail.com>
2026-03-06 21:25:46 +0100

    feat(i18n): implemented translations in Journey and Timeline components

--- src/components/journey/journey.tsx
+++ src/components/journey/journey.tsx
@@ -1,8 +1,10 @@
 import Timeline from "@/components/journey/timeline";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
-import React from "react";
 
 const Journey = () => {
+  const t = useTranslations("journey");
+
   return (
     <section className="flex flex-col gap-4 scroll-mt-20" id="journey">
       <div className="flex gap-2 items-center lg:gap-3">
@@ -13,7 +15,7 @@ const Journey = () => {
           src="journey.svg"
           alt="Journey logo"
         />
-        <h3>Parcours</h3>
+        <h3>{t("sectionTitle")}</h3>
       </div>
       <Timeline />
     </section>
