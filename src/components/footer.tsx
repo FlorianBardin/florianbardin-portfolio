286d4d8b4d67dc5918e7b8ec830e164844564e18
Florian Bardin <fbardin51@gmail.com>
2026-03-07 22:30:34 +0100

    feat(i18n): added translations in Footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import { useTranslations } from "next-intl";
 
 const Footer = () => {
+  const t = useTranslations("footer");
+
   return (
     <footer className="flex justify-center items-center border-t border-border">
       <p className="text-muted-foreground p-6 pb-25 md:pb-6">
-        &#169; 2025 Florian Bardin. All rights reserved.
+        {t("copyright")}
       </p>
     </footer>
   );
