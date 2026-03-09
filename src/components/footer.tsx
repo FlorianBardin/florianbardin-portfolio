0c680e1d8cd11f767273dbb01fb646cfeb967ef4
Florian Bardin <fbardin51@gmail.com>
2026-03-09 20:15:12 +0100

    feat(footer): integrated LegalNotices component into footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,5 @@
 import { useTranslations } from "next-intl";
+import LegalNotices from "./legal-notices";
 
 const Footer = () => {
   const t = useTranslations("footer");
@@ -6,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="flex justify-center items-center border-t border-border">
       <p className="text-muted-foreground p-6 pb-25 md:pb-6">
-        {t("copyright")}
+        {t("copyright")} • <LegalNotices />
       </p>
     </footer>
   );
