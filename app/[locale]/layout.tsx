1898991cc3344f72c7a3aa02e811b37cd50b340f
Florian Bardin <fbardin51@gmail.com>
2026-08-27 18:07:15 +0200

    refactor(metadata): update description

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -14,7 +14,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Florian Bardin",
   description:
-    "Je m'appelle Florian Bardin, passioné par le Cloud et le New Space, futur élève ingénieur à l'IMT Atlantique.",
+    "Je m'appelle Florian Bardin, passioné par le Cloud, élève ingénieur à l'IMT Atlantique.",
 };
 
 export default async function RootLayout({
